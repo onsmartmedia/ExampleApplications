c3560c37d93cc17e220980454d59df2ffc641cf1
Yi <louis.lamgo@gmail.com>
2016-06-08 12:47:35 -0400

    first commit

--- HelloWorld/app/src/main/java/com/videri/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/videri/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package com.videri.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +12,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * TODO: handle all removing/pausing thread in onPause
+     * Ex: timer, runnable, thread, bus
+     *     if(timer != null)
+     *          timer.cancel();
+     *     if(runnable != null)
+     *          handler.removeCallbacks(runnable);
+     *     if(bus != null)
+     *          bus.unregister(this);
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    /**
+     * TODO: handle all resuming thread in onResume()
+     *     if(timer == null)
+     *          timer.start();
+     *     if(runnable != null)
+     *          handler.post(runnable);
+     *     if(bus != null)
+     *          bus.register(this);
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+Example Projects
+=========
+This repo is created to demostrate example projects run on our VLE enviroment. 
