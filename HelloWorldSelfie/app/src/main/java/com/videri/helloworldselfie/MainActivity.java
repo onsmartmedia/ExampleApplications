3ed75b233393cd53017cf46d43939f88023e7c86
Ayal Fieldust <Ayalus@Ayals-MacBook-Pro.local>
2016-06-12 16:02:08 -0400

    first commit

--- HelloWorldSelfie/app/src/main/java/com/videri/helloworldselfie/MainActivity.java
+++ HelloWorldSelfie/app/src/main/java/com/videri/helloworldselfie/MainActivity.java
@@ -1,13 +1,58 @@
 package com.videri.helloworldselfie;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
+
+    private final String TAG = "MainActivity";
+
+
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+
+    TakePictureFragment F1;
+    ShowPictureFragment F2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentManager = getFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        F1 = new TakePictureFragment();
+        F2 = new ShowPictureFragment();
+
+        fragmentTransaction.replace(R.id.content_layout, F1);
+        fragmentTransaction.commit();
+
     }
+
+
+    public void changeFragment(int i) {
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (i == 1) {
+            fragmentTransaction.replace(
+                    R.id.content_layout, F1);
+        } else {
+            fragmentTransaction.replace(
+                    R.id.content_layout, F2);
+        }
+        fragmentTransaction.commit();
+
+    }
+
+
+    public void respond(String data) {
+        Log.v(TAG, "Calling respond: " + data);
+        //       F2.changeImagePath(data);
+    }
+
 }
